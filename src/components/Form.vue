97d4b4b16170cdd6e0c0d75701c3c731855872e5
IbtissamNoukta <ibtissam.noukta@gmail.com>
2023-02-19 16:05:23 +0100

    state posts and add post

---
+++ src/components/Form.vue
@@ -0,0 +1,63 @@
+<script setup>
+import { reactive } from 'vue'
+import { useCounterStore } from '@/stores/counter'
+import moment from 'moment';
+import { v4 as uuidv4 } from 'uuid';
+
+//get store
+//{}=>distractring no need to use posts.addPost
+const { addPost } = useCounterStore()
+
+//set data
+const data = reactive({
+    post : {
+        username : '',
+        body : ''
+    }
+})
+
+//add post
+const storePost = () => {
+    const post = data.post;
+    post.id = uuidv4();
+    post.time = moment().format('MMMM Do YYYY, h:mm:ss a');
+    addPost(post);
+    data.post = {
+        username : '',
+        body : ''
+    }
+
+}
+</script>
+
+<template>
+    <div class="card mt-2">
+        <div class="card-body">
+          <form> 
+            <div class="mb-3">
+                <label for="name" class="form-label">Username</label>
+
+                <input v-model="data.post.username" 
+                type="text" class="form-control" 
+                id="name" placeholder="name">
+            </div>
+            <div class="mb-3">
+                <label for="post" class="form-label">Post</label>
+
+                <textarea v-model="data.post.body" 
+                class="form-control" id="post" rows="3" 
+                placeholder="Post"></textarea>
+            </div>
+
+            <button :disabled="!data.post.username || !data.post.body"
+            @click.prevent="storePost"
+            type="submit" class="btn btn-primary">Share</button>
+        </form>  
+        </div>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,12 +1,15 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
+export const useCounterStore = defineStore('posts', () => {
+//state
+  // we use local storage if we refresh (actualiser) page data document't go
+  //kn7wloh mn json ljs object
+  const posts = ref(JSON.parse(localStorage.getItem('posts')) || []);
+//action
+  function addPost(post){
+    this.posts.unshift(post);
+    localStorage.setItem("posts", JSON.stringify(this.posts));
   }
-
-  return { count, doubleCount, increment }
+  return { posts, addPost }
 })
